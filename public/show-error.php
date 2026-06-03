0f2226e76e3ec33bebeaccea4337524e6c9b5f61
Great Onweazu <greatonweazu48@gmail.com>
2026-06-03 10:00:39 +0100

    Clean up temporary diagnostic script public/show-error.php

--- public/show-error.php
+++
@@ -1,8 +0,0 @@
-<?php
-$path = __DIR__ . '/../storage/logs/error.txt';
-if (file_exists($path)) {
-    header('Content-Type: text/plain');
-    echo file_get_contents($path);
-} else {
-    echo "No error file found.";
-}
